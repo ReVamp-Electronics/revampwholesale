afd4b879a903fbd4b9a9214e902a142441b33c44
Tim <j.tim.nolan@gmail.com>
2017-05-25 15:04:46 -0500

    Update Config Export

--- export_config.sh
+++ export_config.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
+# Confirm permissions
+if [ $USER != 'devrw' ]; then
+    echo "Please run as user devrw"
+    exit 1
+fi
 
-php bin/magento config:data:export --filename="/../../config/base" --hierarchical
+# Set CWD to dir of script
+cd "$(dirname "$0")"
+php bin/magento config:data:export --filename="/../../config/store/base/general" --hierarchical --filePerNameSpace
