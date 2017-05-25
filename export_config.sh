ff886a7acc78e9a1ab117bb77003c3a84a12e07e
Tim <j.tim.nolan@gmail.com>
2017-05-25 15:07:54 -0500

    Remove server checks
    Not needed because this script shouldn't be run on the server, just the dev

--- export_config.sh
+++ export_config.sh
@@ -1,10 +1,3 @@
 #!/bin/bash
-# Confirm permissions
-if [ $USER != 'devrw' ]; then
-    echo "Please run as user devrw"
-    exit 1
-fi
 
-# Set CWD to dir of script
-cd "$(dirname "$0")"
-php bin/magento config:data:export --filename="/../../config/store/base/general" --hierarchical --filePerNameSpace
+php ./bin/magento config:data:export --filename="/../../config/store/base/general" --hierarchical --filePerNameSpace
